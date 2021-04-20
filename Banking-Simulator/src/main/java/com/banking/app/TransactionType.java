cb1328f3e46ea301fd2a4b553a7be638addbcb93
wayjay10 <77541629+wayjay10@users.noreply.github.com>
2021-04-20 19:10:09 -0400

    Delete Banking-Simulator directory

--- Banking-Simulator/src/main/java/com/banking/app/TransactionType.java
+++
@@ -1,9 +0,0 @@
-package com.banking.app;
-
-/** A list of the possible types of transactions in the banking simulation.
- * @author wpollock
- *
- */
-public enum TransactionType {
-    DEPOSIT, WITHDRAWAL, INTEREST, CHECK, FEE, PENALTY, ADJUSTMENT;
-}
